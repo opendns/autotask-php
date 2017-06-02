840ee3fbdc15606b49871da9f62ce8d57a069b8f
Eric Reynolds <reynolek@gmail.com>
2017-06-02 12:24:56 -0700

    Use DIRECTORY_SEPARATOR instead of assuming unix

--- src/autoload.php
+++ src/autoload.php
@@ -1,11 +1,12 @@
 <?php
 
 function atws_autoload($class) {
+    $dirSep = DIRECTORY_SEPARATOR;
     $namespaceParts = explode('\\', $class);
     $className = array_pop($namespaceParts);
 
     array_shift($namespaceParts);
-    require_once __DIR__ . '/' . implode('/', $namespaceParts) . "/{$className}.php";
+    require_once __DIR__ . $dirSep . implode($dirSep, $namespaceParts) . "{$className}.php";
 }
 
 spl_autoload_register('atws_autoload');
