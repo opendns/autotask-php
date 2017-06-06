5a1e099433430eef21700b957f134d5c10b896d2
Eric Reynolds <reynolek@gmail.com>
2017-06-06 08:48:12 -0700

    Add in missing directory separator

--- src/autoload.php
+++ src/autoload.php
@@ -6,7 +6,7 @@ function atws_autoload($class) {
     $className = array_pop($namespaceParts);
 
     array_shift($namespaceParts);
-    require_once __DIR__ . $dirSep . implode($dirSep, $namespaceParts) . "{$className}.php";
+    require_once __DIR__ . $dirSep . implode($dirSep, $namespaceParts) . "{$dirSep}{$className}.php";
 }
 
 spl_autoload_register('atws_autoload');
